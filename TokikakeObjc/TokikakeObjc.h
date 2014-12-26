7b20fb2a94b5b8791f4a30b7f9d5e711558a4d77
asai <asai@na-s.jp>
2014-12-26 14:56:00 +0900

    Initial Commit

---
+++ TokikakeObjc/TokikakeObjc.h
@@ -0,0 +1,19 @@
+//
+//  TokikakeObjc.h
+//  TokikakeObjc
+//
+//  Created by yushan on 2014/12/26.
+//  Copyright (c) 2014年 yukiame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TokikakeObjc.
+FOUNDATION_EXPORT double TokikakeObjcVersionNumber;
+
+//! Project version string for TokikakeObjc.
+FOUNDATION_EXPORT const unsigned char TokikakeObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TokikakeObjc/PublicHeader.h>
+
+
